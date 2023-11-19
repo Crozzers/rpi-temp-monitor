cabc82a8b0440bcb2113d218ad8413dd65d3002d
Crozzers <captaincrozzers@gmail.com>
2023-11-19 19:35:49 +0000

    Setup script tweaks
    Put scripts in same dir as setup script and fix rc.local having script added after `exit 0`

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -13,12 +13,14 @@ pip install w1thermsensor
 # Enable w1 therm kernel modules
 echo "dtoverlay=w1-gpio" | sudo tee -a /boot/config.txt
 
+DIR=$(pwd)
+
 # Make program dir
-mkdir -p ~/sensor && cd ~/sensor
+mkdir -p $DIR/sensor && cd $DIR/sensor
 # Fetch program
 wget https://raw.githubusercontent.com/Crozzers/rpi-temp-monitor/main/temp-monitor.py
 
 # Edit rc.local to add as startup script
-echo "python $HOME/sensor/temp-monitor.py &" | sudo tee -a /etc/rc.local
+sed -i "`wc -l < /etc/rc.local`i\\python $DIR/sensor/temp-monitor.py &\\" /etc/rc.local
 
 sudo reboot
